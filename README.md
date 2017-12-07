40e9bb12e11c88a0b720319d4f3d1b832e5a3725
Roy, Agnimitra <Agnimitra.Roy@ellucian.com>
2017-12-08 03:18:29 +0530

    - grid to canvas - readme - pesky favicon

---
+++ README.md
@@ -0,0 +1,3 @@
+Environment:
+ - NodeJS 6.11.1 LTS
+ - macOS Sierra 10.12.6
--- views/index.js
+++ views/index.js
@@ -1,2 +1,38 @@
 console.log('Game of Life');
 
+var socket = io.connect('http://localhost:3000');
+var grid_size;
+var pixel_size;
+var canvas;
+var context;
+
+socket.on('connect', function () {
+    socket.send('new user connected');
+});
+
+socket.on('grid', function (new_grid) {
+    console.log(new_grid);
+    renderGrid(new_grid.grid);
+});
+
+function renderPixel(x, y, state) {
+    context.beginPath();
+    context.rect(x * pixel_size, y * pixel_size, pixel_size, pixel_size);
+    context.fillStyle = state == 1 ? 'black' : 'white';
+    context.fill();
+}
+
+function renderGrid(grid_data) {
+    grid_size = grid_data.length;
+    pixel_size = 20;
+    canvas = document.getElementById('gol_canvas');
+    canvas.width = pixel_size * grid_size;
+    canvas.height = pixel_size * grid_size;
+    context = canvas.getContext('2d');
+
+    for (var row = 0; row < grid_size; row++) {
+        for (var col = 0; col < grid_size; col++) {
+            renderPixel(row, col, grid_data[row][col]);
+        }
+    }
+}
